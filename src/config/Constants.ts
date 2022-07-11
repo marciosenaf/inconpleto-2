34c96fcff724dbd737580b6a5c228bdb207e8800
marciosenaf <tinerole10@gmail.com>
2022-07-11 10:45:18 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {  
+    API_URL : "http://localhost:5000",
+   MONGO_CONNECTION: 
+'mongodb+srv://usuario:usuario@cluster0.oijbn.mongodb.net/encurtador d URL?retryWrites=true&w=majority'
+}
